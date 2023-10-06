55937a4fb72d37adbe3b70341abd3c7917b57b4e
aryankatiyar123 <aryan.katiyar@nagarro.com>
2023-10-06 14:14:20 +0530

    first commit

---
+++ src/test/java/jenkinsMavenIntegration/SampleTestCase.java
@@ -0,0 +1,32 @@
+package jenkinsMavenIntegration;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class SampleTestCase 
+{
+	WebDriver driver;
+	
+	@Parameters("browser")
+	@Test
+	public void lauchBrowser(String Launching_browser)
+	{		
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		System.out.println(driver.getTitle());
+		
+		Assert.assertTrue(driver.getTitle().contains("Google"),"Title match");
+		
+		driver.quit();
+	}
+
+}
